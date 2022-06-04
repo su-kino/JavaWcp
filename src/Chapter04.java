23910b56e712a0295154a973fb0795e47a28a9ec
su-kino <97873142+su-kino@users.noreply.github.com>
2022-06-04 15:05:42 +0900

    [practice]~4章

---
+++ src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "sugi";
+		System.out.println(str);
+		str = "yama";
+		System.out.println(str);
+		int NUM = 24;
+		System.out.println(NUM);
+	}
+}
